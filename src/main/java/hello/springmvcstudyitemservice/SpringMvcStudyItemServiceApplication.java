7f0f55fcb0d6aa5926f33d44a713d36f003a12dc
JuJinPark <wnwls1216@gmail.com>
2021-07-26 23:17:15 +0900

    [feature] 프로젝트 구조 생성

---
+++ src/main/java/hello/springmvcstudyitemservice/SpringMvcStudyItemServiceApplication.java
@@ -0,0 +1,13 @@
+package hello.springmvcstudyitemservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcStudyItemServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcStudyItemServiceApplication.class, args);
+	}
+
+}
